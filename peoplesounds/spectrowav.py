e1e309162d3a812062c0eb8661547bd05b6bb363
Daniel Corson <danl@alum.mit.edu>
2014-06-04 15:53:53 -0400

    lies in spectrowav.py

--- peoplesounds/spectrowav.py
+++ peoplesounds/spectrowav.py
@@ -6,20 +6,14 @@ from numpy import *
 import scikits.audiolab as audiolab
 import struct
 
-def show_Specgram(speech):
-    '''
-    Reads .wav file from STDIN and plots the spectrogram
-    '''
-    sound = audiolab.sndfile(speech,'read')
-    # Reads wav file with audiolab
+def show_specgram(speech):
+    sound = audiolab.sndfile(speech, 'read')
     sound_info = sound.read_frames(sound.get_nframes())
-    # Extracts feature info from sound file with scipy module
     spectrogram = specgram(sound_info)
-    #Generates soectrogram with matplotlib specgram
     title('Spectrogram of %s'%sys.argv[1])
     show()
     sound.close()
     return spectrogram
 
 file = sys.argv[1]
-show_Specgram(file)
+show_specgram(file)
